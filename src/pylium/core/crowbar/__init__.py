1047b023877c9ef775453cf49e06a7b1d1969c45
Rouven Raudzus <rouven.raudzus@gmail.com>
2025-06-21 19:05:18 +0200

    Remove unused dependency exports from Crowbar module to streamline the codebase and improve clarity.

--- src/pylium/core/crowbar/__init__.py
+++ src/pylium/core/crowbar/__init__.py
@@ -1,7 +1,6 @@
 from .__header__ import Crowbar, __manifest__, list_dependencies, pyproject_update, list_dependencies2
-from .data import DependencyInfo, ConflictInfo, DependencyStats, DependencyAnalysis
 
 __all__ = [
-    'Crowbar', '__manifest__', 'list_dependencies', 'pyproject_update', 'list_dependencies2',
-    'DependencyInfo', 'ConflictInfo', 'DependencyStats', 'DependencyAnalysis'
+    'Crowbar', '__manifest__', 'list_dependencies', 'pyproject_update', 'list_dependencies2'
+    
 ] 
